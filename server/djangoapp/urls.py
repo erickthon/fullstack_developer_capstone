84fe176891c7c7764b868dba52cd2fe8d8aabc53
erickthon <erickphez@gmail.com>
2024-02-15 17:22:25 -0600

    Add Django models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,19 +5,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for registering a new user
     path(route='registration', view=views.registration, name='registration'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
